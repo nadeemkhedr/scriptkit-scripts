6a73cd62e4b4eea49ba9646eecf928d102a67f6a
Nadeem Khedr <nadeemkhedr@gmail.com>
2021-08-28 01:06:06 +0200

    ✨ feat(yabai): remove abs path for yabai, fixed karabiner issue

--- yabai-zen-toggle.js
+++ yabai-zen-toggle.js
@@ -1,13 +1,23 @@
-// Menu: Yabai Zen Toggle
+// Menu: yabai Zen Toggle
 // Description: Toggle Zen mode in yabai by increasing the padding left/right
 // Author: Nadeem Khedr
 // Twitter: @nadeemkhedr
 
-const selectedSpace = exec(`yabai -m query --spaces | jq '.[] | select(.visible==1) | .label'`).trim()
-const rightPadding = exec(`yabai -m config --space ${selectedSpace} right_padding`).trim()
+const selectedSpace = exec(
+  `yabai -m query --spaces | jq '.[] | select(.visible==1) | .label'`,
+  { silent: true }
+).trim()
+const rightPadding = exec(
+  `yabai -m config --space ${selectedSpace} right_padding`,
+  { silent: true }
+).trim()
 
-if(rightPadding === '15') {
-  exec(`yabai -m space --padding abs:15:15:700:700`)
+if (rightPadding === '15') {
+  exec(`yabai -m space --padding abs:15:15:700:700`, {
+    silent: true,
+  })
 } else {
-  exec(`yabai -m space --padding abs:15:15:15:15`)
+  exec(`yabai -m space --padding abs:15:15:15:15`, {
+    silent: true,
+  })
 }
